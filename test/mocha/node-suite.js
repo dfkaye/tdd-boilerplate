c9bc96f2f1a78c2de70a48c6e784d728bf99e4a9
david kaye <dfkaye@yahoo.com>
2014-03-10 13:53:07 -0700

    white-space and fake assert/boilerplate for doc example

--- test/mocha/node-suite.js
+++ test/mocha/node-suite.js
@@ -5,10 +5,13 @@
 
 //Load mocha
 var Mocha = require("mocha");
+
 //Tell mocha to use the interface.
 var mocha = new Mocha({ui:"bdd", reporter:"spec"});
+
 //Add your test files
 mocha.addFile("./test/mocha/suite.js");
+
 //Run your tests
 mocha.run(function(failures){
   process.exit(failures);
--- test/mocha/suite.js
+++ test/mocha/suite.js
@@ -5,12 +5,14 @@ var assert;
 
 if (typeof require == 'function') {
   // enable to re-use in a browser without require.js
+  //boilerplate = 'boilerplate';
   boilerplate = require('../../index.js');
   assert = require('assert');
 }
 
 describe('smoke test', function() {
   it('should pass', function () {
+    //assert.equal(boilerplate, 'boilerplate');
     assert.equal(boilerplate(), 'boilerplate', 'boilerplate');
   });
 });
