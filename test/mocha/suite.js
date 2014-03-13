59ad2e718cb323d10993b899a731e0fb1359b302
david kaye <dfkaye@yahoo.com>
2014-03-13 12:15:31 -0700

    rem comments in mocha test

--- test/mocha/suite.js
+++ test/mocha/suite.js
@@ -5,14 +5,12 @@ var assert;
 
 if (typeof require == 'function') {
   // enable to re-use in a browser without require.js
-  //boilerplate = 'boilerplate';
   boilerplate = require('../../index.js');
   assert = require('assert');
 }
 
 describe('smoke test', function() {
-  it('should pass', function () {
-    //assert.equal(boilerplate, 'boilerplate', 'failure message');
+  it('should pass', function () { 
     assert.equal(boilerplate(), 'boilerplate', 'failure message');
   });
 });
